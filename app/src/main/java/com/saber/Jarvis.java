6e1de0709b00574f6af49ec741ef011cca0c7f9a
hanjianqiao <hanjianqiao@gmail.com>
2017-10-27 02:02:42 +0800

    update: add rn to jarvis

--- app/src/main/java/com/saber/Jarvis.java
+++ app/src/main/java/com/saber/Jarvis.java
@@ -1,11 +1,14 @@
 package com.saber;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by lct on 2017/10/27.
@@ -14,7 +17,22 @@ import android.view.ViewGroup;
 public class Jarvis extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        View v = inflater.inflate(R.layout.jarvis_layout, container, false);
-        return v;
+        View view = inflater.inflate(R.layout.jarvis_layout, container, false);
+
+        Button btn = (Button)view.findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), Main2Activity.class);
+                startActivity(i);
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i("Right", "onCreate()");
     }
 }
